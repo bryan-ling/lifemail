a73bfcc59bbbc93646b9d14d9c29405fba2874d0
temp0w <temp0w@yahoo.ca>
2016-12-12 14:21:16 -0500

    first draft
    plz work

---
+++ lifemail.js
@@ -0,0 +1,11 @@
+function main(){
+	$('.planSingle').hover(function(){
+		$(this).find('.planHidden').slideToggle(250);
+		$(this).find('.plansrow #familyLogo').toggleClass('familyLogohover');
+		
+	});
+	
+	
+};
+
+$(document).ready(main);
